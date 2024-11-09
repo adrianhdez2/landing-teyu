b05c5a8bf65778d164c4dd5403905588de160201
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-11-08 18:22:21 -0600

    add types and data

---
+++ src/components/utils/data.ts
@@ -0,0 +1,24 @@
+import type { About } from "./types";
+
+export const ITEMS_ABOUT: About[] = [
+    {
+        title: 'Visionario Digital',
+        description: '¿Tienes una idea innovadora y quieres transformarla en una aplicación que impacte? Nosotros convertimos tus conceptos en experiencias digitales atractivas y funcionales, ayudándote a marcar la diferencia en el mercado y conectarte con tu audiencia de manera única.',
+        img: 'customer.png'
+    },
+    {
+        title: 'Emprendedor en Expansión',
+        description: 'Si buscas hacer crecer tu negocio con tecnología eficiente, nuestras soluciones están diseñadas para optimizar tus procesos, mejorar la experiencia de tus clientes y maximizar tu alcance. Llevamos tu visión al siguiente nivel con plataformas que funcionan y destacan.',
+        img: 'market-analysis.png'
+    },
+    {
+        title: 'Consultor en Búsqueda de Impacto',
+        description: 'Como experto en tu industria, sabes que una plataforma robusta y confiable marca la diferencia. Nosotros creamos aplicaciones personalizadas que te permiten conectar con tus clientes, automatizar procesos y brindar tus servicios de forma profesional y sin complicaciones.',
+        img: 'businessman.png'
+    },
+    {
+        title: 'Empresa en Transformación Digital',
+        description: 'Si tu negocio necesita modernizarse y adaptarse a la era digital, te ofrecemos soluciones que integran lo último en tecnología para que puedas mejorar la eficiencia, reducir costos y alcanzar a más clientes. Nos encargamos de transformar tus procesos, asegurando que tu empresa esté lista para el futuro.',
+        img: 'consulting.png'
+    },
+]
